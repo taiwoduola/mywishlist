bc36b2ece374b6b4b229b0cb771742a4cb8ce19f
taiwoduola <61692734+taiwoduola@users.noreply.github.com>
2020-07-29 12:22:59 +0100

    created the trends component and the styling

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+import TrendsContainer from '../../components/trendscontainer/trendscontainer.component';
+
+const Homepage = () => (
+	<div className="homepage">
+		<TrendsContainer />
+	</div>
+);
+
+export default Homepage;
